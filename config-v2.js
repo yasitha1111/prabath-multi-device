e75875e6e0907566f7968489eee49c039fd3ba92
yasitha1111 <lakshany398@gmail.com>
2025-07-08 16:11:22 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const DATABASE = "" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
-const BOT_NUMBER = "" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
-const SESSION_ID = "" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
+const DATABASE = "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" // මෙතනට mongodb,mysql,postgresql or github token එකක් enter කරන්න මේ "" දෙක මැදට. (Enter a mongodb,mysql,postgresql or github token here between these two "".)
+const BOT_NUMBER = "94772685293" // ඔයාගෙ whatsapp number එක. (your whatsapp number)
+const SESSION_ID = "PRABATH-MD~sQMh7iPFUkrFF15" // ඔයාගෙ session id එක මේකෙන් ගන්න - https://prabathmd-official.vercel.app
 
 
 // ඉහත ඒව "" මැදට එකතු කරන්න වෙනත් කිසිම දෙයක් වෙනස් නොකරන්න ❌. (Add "" in the middle of the above and ❌ don't change anything else.)
